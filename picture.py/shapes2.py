30ab2bf96963270adc6289812125258eff1cfdc0
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-09-05 16:52:11 +0000

    Working with Python Programming

---
+++ picture.py/shapes2.py
@@ -0,0 +1,33 @@
+/*
+ * triangle() - Triangle function
+ * square() - Square function
+ * main() - Main Function
+ */
+
+def triangle():
+    # To print a triangle
+    print("THIS IS A TRIANGLE\n")
+    print("   *")
+    print("  ***")
+    print(" *****")
+    print("*******")
+
+def square():
+    # To print a square
+    print("THIS IS A SQUARE\n")
+    print("* * * *")
+    print("* * * *")
+    print("* * * *")
+    print("* * * *")
+
+def main():
+    # To print a triangle
+    triangle()
+    print()
+    # To print a square
+    square()
+
+if __name__=='__main__':
+    main()
+print("\nEnd of program")
+
